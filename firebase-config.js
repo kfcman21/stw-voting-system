1e422f235c8d59111fa4d3c286d790c795d23b29
CHAN KYU PARK <kfcman21@naver.com>
2025-07-09 15:50:16 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,10 @@
 // Firebase 설정 파일
 // 실제 사용 시 이 파일의 설정을 업데이트하세요
 
-const firebaseConfig = {
+// Firebase 설정 파일
+// 실제 사용 시 이 파일의 설정을 업데이트하세요
+
+window.firebaseConfig = {
     // Firebase Console에서 가져온 설정 정보를 여기에 입력하세요
     apiKey: "AIzaSyC5N-5bXkPzzXkZYBkznlbXWzrd3snGXB0",
     authDomain: "stw-voting-system.firebaseapp.com",
@@ -11,7 +14,4 @@ const firebaseConfig = {
     messagingSenderId: "859381962769",
     appId: "1:859381962769:web:1401c0e7c71e5feeacf9b0",
     measurementId: "G-2H02P62CSZ"
-};
-
-// 설정을 전역으로 내보내기
-window.firebaseConfig = firebaseConfig; 
+}; 
